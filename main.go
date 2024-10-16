349f540d43fc1debedac1a613bdbf20ad16a6ecd
Smith Ringtho <sringtho@gmail.com>
2024-10-16 11:44:53 +0300

    Added connection to database

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sringtho/go_crud/initializers"
 )
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-		"message":"Smith Ringtho",},
+		"message":"Smith Ringtho J",},
 		)})
 	
-	router.Run(":3000")
+	router.Run()
 }
