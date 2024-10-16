013d4e1ef03547af9fc1028781ad845d5b745006
Smith Ringtho <sringtho@gmail.com>
2024-10-16 12:11:23 +0300

    Added table migrate for postgres and create post

---
+++ controllers/postsController.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sringtho/go_crud/initializers"
+	"github.com/sringtho/go_crud/models"
+)
+
+func CreatePost(c *gin.Context) {
+	// Get Data off request body
+	var body struct {
+		Body string
+		Title string
+	}
+
+	if err := c.Bind(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to bind data",
+		})
+	}
+
+	// Create a post
+	post := models.Post{Title: body.Title, Body: body.Body}
+	result := initializers.DB.Create(&post)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to create post",
+		})
+		return
+	}
+
+	// return
+	c.JSON(http.StatusOK, gin.H{
+		"post": post,
+	})
+
+}
--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/sringtho/go_crud/controllers"
 	"github.com/sringtho/go_crud/initializers"
+	"github.com/sringtho/go_crud/models"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
+	
 }
 
 func main() {
-	router := gin.Default()
-
-	router.POST("/posts", controllers.CreatePost) 
-	
-	router.Run()
+	initializers.DB.AutoMigrate(&models.Post{})
 }
